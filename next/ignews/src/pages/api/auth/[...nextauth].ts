81a864f65d80d9867878b50adee0d613675f35cc
Júnior Albuquerque <juioralbuquerque81@gmail.com>
2022-07-24 23:54:05 -0300

    chore: add fauna db

--- next/ignews/src/pages/api/auth/[...nextauth].ts
+++ next/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb'
+import { Fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,10 +11,47 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: 'read:user'
-        }
-      }
+          scope: "read:user",
+        },
+      },
     }),
     // ...add more providers here
   ],
-})
+  secret: process.env.JWT_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+
+      try {
+        await Fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: { email }
+              }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+  
+        return true;
+      } catch {
+        return false
+      }
+    },
+  },
+});
